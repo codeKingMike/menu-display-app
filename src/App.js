0009217bee59ac1f60f72778bbcaf010d579b844
codeKingMike <codekingmike@gmail.com>
2021-03-30 20:40:16 -0700

    making a menu app with react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import items from './data';
+import Categories from './Categories';
+import Menu from './Menu';
+
+
+function App() {
+  return <h2>Menu Display App</h2>
+}
+
+export default App;
