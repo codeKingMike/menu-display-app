e566f326f7af126aea7f37d700035e2fd48ea4a2
codeKingMike <codekingmike@gmail.com>
2021-03-31 21:34:42 -0700

    made a change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import Menu from './Menu';
 
 
 function App() {
-  return <h2>Menu Display App</h2>
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
