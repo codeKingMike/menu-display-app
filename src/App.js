647fed11b0a1aaf6312d4b0062fc8ef57f162825
codeKingMike <codekingmike@gmail.com>
2021-03-31 22:37:19 -0700

    finished the last part of the functionality all by myself without the tutorial to see if I had the hang of react and I got the code correct :)

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import items from './data';
 import Categories from './Categories';
 import Menu from './Menu';
 
+const allCategories = ['all', ... new Set(items.map((item) => item.category))];
+console.log(allCategories);
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   return (
     <main>
       <section className="menu section">
@@ -14,7 +25,7 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
--- src/Categories.js
+++ src/Categories.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const Categories = () => {
-    return <h2>Categories component</h2>
+const Categories = ({ filterItems, categories }) => {
+    return (
+        <div className="btn-conatiner">
+            {categories.map((categorie, index) => {
+                return <button key={index} className='filter-btn' onClick={() => filterItems(categorie)}>{categorie}</button>
+            })}
+        </div>
+    );
 }
 
 export default Categories;
